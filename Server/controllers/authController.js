598302e852d5c46daf475f7535fcb9cf4cdcea5e
Er_Udhay Chourasia <udaychourasia29@gmail.com>
2026-05-17 17:51:57 +0530

    Fixed deployment

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -47,7 +47,7 @@ const signup = async (req, res) => {
         id: user._id,
       },
 
-      "secretkey",
+      process.env.JWT_SECRET,
 
       {
         expiresIn: "7d",
@@ -121,7 +121,7 @@ const login = async (req, res) => {
         id: user._id,
       },
 
-      "secretkey",
+      process.env.JWT_SECRET,
 
       {
         expiresIn: "7d",
